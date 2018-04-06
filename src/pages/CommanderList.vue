df77976f4d66fb6e1123e69bad3c0424c52adb42
tri-chandra <tri-chandra@users.noreply.github.com>
2018-04-06 16:29:28 +1000

    Added formatting to commander info card

--- src/pages/CommanderList.vue
+++ src/pages/CommanderList.vue
@@ -25,9 +25,49 @@
     <v-card>
       <v-card-title class="headline">Commander Details</v-card-title>
       <v-card-text>
-        <p>{{selectedCommander.name}}</p>
-        <p>{{selectedCommander.class}}</p>
-        <p>{{selectedCommander.banner}}</p>
+        <v-container fluid>
+          <v-layout row wrap>
+            <v-flex xs6>
+              <v-avatar :tile="true" :size="48">
+                <img src="/static/img/icons/favicon-32x32.png">
+              </v-avatar>
+              <p>{{selectedCommander.name}}</p>
+              <p>{{selectedCommander.class}}</p>
+              <p>{{selectedCommander.banner}}</p>
+              <p>{{selectedCommander.weapon}}</p>
+              <p>{{selectedCommander.armor}}</p>
+              <p>{{selectedCommander.accessory}}</p>
+              <p>TERRAIN</p>
+            </v-flex>
+            <v-flex xs6>
+              <v-list two-line>
+                <v-subheader>Set 1</v-subheader>
+                <v-list-tile v-for="(item, idx) in selectedCommander.skills.set1" :key="idx">
+                  <v-list-tile-avatar :tile="true">
+                    <img src="/static/img/icons/favicon-32x32.png">
+                  </v-list-tile-avatar>
+                  <v-list-tile-content>
+                    <v-list-tile-title>{{item}}</v-list-tile-title>
+                    <v-list-tile-sub-title>{{item}}</v-list-tile-sub-title>
+                  </v-list-tile-content>
+                </v-list-tile>
+
+                <v-subheader>Set 2</v-subheader>
+                <v-list-tile v-for="(item, idx) in selectedCommander.skills.set2" :key="idx">
+                  <v-list-tile-avatar :tile="true">
+                    <img src="/static/img/icons/favicon-32x32.png">
+                  </v-list-tile-avatar>
+                  <v-list-tile-content>
+                    <v-list-tile-title>{{item}}</v-list-tile-title>
+                    <v-list-tile-sub-title>{{item}}</v-list-tile-sub-title>
+                  </v-list-tile-content>
+                </v-list-tile>
+              </v-list>
+            </v-flex>
+          </v-layout>
+        </v-container>
+
+
         <p>{{selectedCommander.meta && selectedCommander.meta.stages}}</p>
       </v-card-text>
       <v-card-actions>
@@ -49,8 +89,29 @@ export default {
         {text: 'Class', sortable: true, value: 'class'},
         {text: 'Banner', sortable: true, value: 'banner'}
       ],
-      showDetail: false,
-      selectedCommander: {}
+      showDetail: true,
+      selectedCommander: {
+        name: 'TEST NAME',
+        class: 'TEST CLASS',
+        banner: 'TEST BANNER',
+        weapon: 'WEAPON',
+        armor: 'ARMOR',
+        accessory: 'ACCESSORY',
+        skills: {
+          set1: [
+            'SKILL 1',
+            'SKILL 2',
+            'SKILL 3',
+            'SKILL 4',
+            'SKILL 5',
+            'SKILL 6',
+          ],
+          set2: [
+            'SET 1',
+            'SET 2'
+          ]
+        }
+      }
     }
   },
   computed: {
